4a381df904629b56893c0fcf295b313001ef5e46
Carla Valdivia <valdivia.carla@gmail.com>
2022-08-09 19:56:40 -0300

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="navbar">
+      <h1 className="navbar--title">Meme Generator</h1>
+      <p className="navbar--paragraph">React Course - Project 3</p>
+    </div>
+  )
+}
